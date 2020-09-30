6f96a2d7e3b13af0bb46725e32fadda1dbe0146d
pniman <70342163+pniman@users.noreply.github.com>
2020-09-30 12:41:45 -0700

    Add files via upload

--- circuit_2.v
+++ circuit_2.v
@@ -26,20 +26,21 @@ module circuit_2#(parameter DATAWIDTH = 32)(Clk, Rst, a, b, c, z, x);
     input [DATAWIDTH-1:0] a, b, c;
     output [DATAWIDTH-1:0] z, x;
     wire [DATAWIDTH-1:0] d, e, f, g, h;
-    wire [1:0] dLTe, dEQe;
+    wire [DATAWIDTH-1:0] dLTe, dEQe;
     wire [DATAWIDTH-1:0] zwire, xwire;
+    wire [DATAWIDTH-1:0] unuseful_GT;
     
-    ADD #(.DATAWIDTH(8)) myAdd1(a, b, d);
-    ADD #(.DATAWIDTH(8)) myAdd2(a, c, e);
-    SUB #(.DATAWIDTH(8)) mySub(a, b, f);
-    COMP #(.DATAWIDTH(8)) myComp1(d, e, dEQe);
-    COMP #(.DATAWIDTH(8)) myComp2(d, e, dLTe);
-    MUX2x1 #(.DATAWIDTH(8)) myMUX2x11(d, e, dLTe, g);
-    MUX2x1 #(.DATAWIDTH(8)) myMUX2x12(g, f, dEQe, h);
-    SHL #(.DATAWIDTH(8)) mySHL(g, dLTe, xwire);
-    SHR #(.DATAWIDTH(8)) mySHR(h, dQEe, zwire);
-    REG #(.DATAWIDTH(8)) myReg1(xwire, Clk, Rst, x);
-    REG #(.DATAWIDTH(8)) myReg2(zwire, Clk, Rst, z);
+    ADD #(.DATAWIDTH(32)) myAdd1(a, b, d);
+    ADD #(.DATAWIDTH(32)) myAdd2(a, c, e);
+    SUB #(.DATAWIDTH(32)) mySub(a, b, f);
+    COMP #(.DATAWIDTH(1)) myComp1(d, e, dEQe, dLTe, unuseful_GT);
+    COMP #(.DATAWIDTH(1)) myComp2(d, e, dEQe, dLTe, unuseful_GT);
+    MUX2x1 #(.DATAWIDTH(32)) myMUX2x11(d, e, dLTe, g);
+    MUX2x1 #(.DATAWIDTH(32)) myMUX2x12(g, f, dEQe, h);
+    SHL #(.DATAWIDTH(32)) mySHL(g, dLTe, xwire);
+    SHR #(.DATAWIDTH(32)) mySHR(h, dQEe, zwire);
+    REG #(.DATAWIDTH(32)) myReg1(xwire, Clk, Rst, x);
+    REG #(.DATAWIDTH(32)) myReg2(zwire, Clk, Rst, z);
     
     
 
