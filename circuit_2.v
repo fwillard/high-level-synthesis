8a8d7b20a2068278c18f17c92d4f9d8d67b430f5
pniman <70342163+pniman@users.noreply.github.com>
2020-10-02 10:26:42 -0700

    Add files via upload

--- circuit_2.v
+++ circuit_2.v
@@ -20,21 +20,20 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module circuit_2#(parameter DATAWIDTH = 32)(Clk, Rst, a, b, c, z, x);
+module circuit_2 (Clk, Rst, a, b, c, x, z);
 
     input Clk, Rst;
-    input [DATAWIDTH-1:0] a, b, c;
-    output [DATAWIDTH-1:0] z, x;
-    wire [DATAWIDTH-1:0] d, e, f, g, h;
-    wire [DATAWIDTH-1:0] dLTe, dEQe;
-    wire [DATAWIDTH-1:0] zwire, xwire;
-    wire [DATAWIDTH-1:0] unuseful_GT;
+    input [31:0] a, b, c;
+    output [31:0] x, z;
+    wire [31:0] d, e, f, g, h;
+    wire [0:0] dLTe, dEQe;
+    wire [31:0] zwire, xwire;
+    wire [31:0] unuseful_GT;
     
     ADD #(.DATAWIDTH(32)) myAdd1(a, b, d);
     ADD #(.DATAWIDTH(32)) myAdd2(a, c, e);
     SUB #(.DATAWIDTH(32)) mySub(a, b, f);
-    COMP #(.DATAWIDTH(1)) myComp1(d, e, dEQe, dLTe, unuseful_GT);
-    COMP #(.DATAWIDTH(1)) myComp2(d, e, dEQe, dLTe, unuseful_GT);
+    COMP #(.DATAWIDTH(32)) myComp1(d, e, unuseful_GT, dLTe, dEQe);
     MUX2x1 #(.DATAWIDTH(32)) myMUX2x11(d, e, dLTe, g);
     MUX2x1 #(.DATAWIDTH(32)) myMUX2x12(g, f, dEQe, h);
     SHL #(.DATAWIDTH(32)) mySHL(g, dLTe, xwire);
